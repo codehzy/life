5b5ca1c82a9f45bf4a96929181cda8a806b8bb9b
codehzy <1257664828@qq.com>
2022-12-01 11:27:44 +0800

    feat(添加Project): ✨ 添加Project

--- src/store/projects.tsx
+++ src/store/projects.tsx
@@ -1,15 +1,12 @@
-import { BiCarousel } from 'react-icons/bi';
-import { CgCodeSlash, CgNotes, CgWebsite } from 'react-icons/cg';
-import { GiSelect } from 'react-icons/gi';
-import { GrTemplate } from 'react-icons/gr';
 import { IoAccessibilitySharp } from 'react-icons/io5';
+import { CgNotes } from 'react-icons/cg';
 import { atom } from 'recoil';
 
 export const projectsAtom = atom({
   key: 'PROJECTS_ATOM',
   default: [
     {
-      category: 'front-around',
+      category: 'Front-around',
       child: [
         {
           title: 'monorepo',
@@ -17,6 +14,18 @@ export const projectsAtom = atom({
             '一个monorepo的小demo，配合https://www.codehzy.cn/blog/04_quick-monorepo博文进行学习',
           link: 'https://github.com/codehzy/monorepo',
           icon: <IoAccessibilitySharp />,
+        },
+      ],
+    },
+    {
+      category: 'Personal-notes',
+      child: [
+        {
+          title: 'MarkLife-Obsidian',
+          description:
+            '一个使用Obsidian维护的记录个人工作和生活的笔记',
+          link: 'https://github.com/codehzy/marklife',
+          icon: <CgNotes />, 
         },
       ],
     },
